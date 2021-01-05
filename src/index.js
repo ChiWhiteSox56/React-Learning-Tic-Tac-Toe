7e9b2270ef0c77bf6b813d6eb0854b7a99e7b380
dpvitt <dapavitt@gmail.com>
2021-01-05 15:58:49 +0000

    Tidy formatting

--- src/index.js
+++ src/index.js
@@ -1,51 +1,50 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function Square(props) {
+function Square({ isWinning, onClick, value }) {
   return (
     <button
-      className={"square " + (props.isWinning ? "square--winning" : null)}
-      onClick={props.onClick}
+      className={"square " + (isWinning ? "square--winning" : null)}
+      onClick={onClick}
     >
-      {props.value}
+      {value}
     </button>
   );
 }
 
-function SquaresList({count, squares, winningSquares, onClick}) {
-
+function SquaresList({ count, squares, winningSquares, onClick }) {
     let squareCount = [];
     for (let i = count; i < count + 3; i++) {
         squareCount.push(i);
     }
 
     return (
-        <div className="board-row"> 
-            {
-                squareCount.map((i) => {
-                    return (
-                    <Square
-                    isWinning={winningSquares.includes(i)}
-                    key={"square " + i}
-                    value={squares[i]}
-                    onClick={() => onClick(i)}
-                  />
-                )})
-            }
-        
-        </div>
+      <div className="board-row"> 
+        {
+          squareCount.map((i) => {
+            return (
+              <Square
+                isWinning={winningSquares.includes(i)}
+                key={"square " + i}
+                value={squares[i]}
+                onClick={() => onClick(i)}
+              />
+            )
+          })
+        }
+      </div>
     )
 }
 
 function Board(props) {
-    return (
-        <div>
-            <SquaresList count={0} {...props} />
-            <SquaresList count={3} {...props} />
-            <SquaresList count={6} {...props} />
-        </div>
-    );
+  return (
+    <div>
+      <SquaresList count={0} {...props} />
+      <SquaresList count={3} {...props} />
+      <SquaresList count={6} {...props} />
+    </div>
+  );
 }
 
 function useHistory() {
@@ -54,27 +53,31 @@ function useHistory() {
           squares: Array(9).fill(null),
         },
       ])
-      return {history, setHistory}
+      return { history, setHistory }
 }
 
 function useSteps() {
-    const [steps, setSteps] = useState({stepNumber:0, xIsNext:true, isWinning:false})
-    return {steps, setSteps}
+    const [steps, setSteps] = useState({
+      stepNumber:0,
+      xIsNext:true,
+      isWinning:false
+    });
+    return { steps, setSteps }
 }
 
 function useOrder() {
     const [order, setOrder] = useState(true)
-    return {order, setOrder}
+    return { order, setOrder }
 }
 
 function Moves({move, steps, desc, jumpTo}) {
-    return (
+  return (
     <li key={move}>
         <button onClick={() => jumpTo(move)}>
           {move === steps.stepNumber ? <b>{desc}</b> : desc}
         </button>
     </li>
-    );
+  );
 }
 
 function Game() {
@@ -82,7 +85,7 @@ function Game() {
     const {steps, setSteps} = useSteps()
     const {order, setOrder} = useOrder()
 
-function handleClick(i) {
+  function handleClick(i) {
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     const locations = [
@@ -101,7 +104,8 @@ function handleClick(i) {
     }
     squares[i] = steps.xIsNext ? "X" : "O";
     setHistory(
-      [...history,
+      [
+        ...history,
         {
           squares: squares,
           loc: locations[i],
@@ -139,7 +143,7 @@ function handleClick(i) {
       : "Go to game start";
 
     return (
-        <Moves move={move} steps={steps} desc={desc} jumpTo={jumpTo}/>
+      <Moves move={move} steps={steps} desc={desc} jumpTo={jumpTo}/>
     );
   });
 
@@ -154,22 +158,22 @@ function handleClick(i) {
 
     return (
         <div className="game">
-        <div className="game-board">
+          <div className="game-board">
             <Board
-            squares={current.squares}
-            onClick={(i) => handleClick(i)}
-            winningSquares={winner ? winner.line : []}
+              squares={current.squares}
+              onClick={(i) => handleClick(i)}
+              winningSquares={winner ? winner.line : []}
             />
-        </div>
-        <div className="game-info">
-            <div>{status}</div>
-            <ol>{order.isDescending ? moves : moves.reverse()}</ol>
-        </div>
-        <div>
+          </div>
+          <div className="game-info">
+              <div>{status}</div>
+              <ol>{order.isDescending ? moves : moves.reverse()}</ol>
+          </div>
+          <div>
             <button onClick={() => sortHistory()}>
-            Sort by: {order.isDescending ? "Descending" : "Ascending"}
+              Sort by: {order.isDescending ? "Descending" : "Ascending"}
             </button>
-        </div>
+          </div>
         </div>
     );
 }
